c8ce6b7e5c50952082195b3fbd9628ef444d63a7
EunChani <kimbh0905@gmail.com>
2018-12-04 21:46:15 +0900

    1.0.00 init

---
+++ app/src/main/java/marty/ration/com/baseapp/utils/Utils.java
@@ -0,0 +1,20 @@
+package marty.ration.com.baseapp.utils;
+
+import android.content.Context;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.widget.Toast;
+
+/**
+ * Created by Charny on 2018-12-04.
+ */
+public class Utils {
+
+    public static void showToast(Context context, String message) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+    }
+    public static void showSnack(BaseActivity context,String message){
+        View rootView = context.findViewById(android.R.id.content);
+        Snackbar.make(rootView,message,Snackbar.LENGTH_SHORT).show();
+    }
+}
